68a51ded60b0e41607500450c3baedd8ca508522
Wei Zhao <zw2253782@gmail.com>
2017-04-14 12:59:25 -0500

    Add the usb serial/UDP service/usbserial.jar

--- app/src/main/java/wisc/selfdriving/activity/MainActivity.java
+++ app/src/main/java/wisc/selfdriving/activity/MainActivity.java
@@ -1,13 +1,23 @@
 package wisc.selfdriving.activity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+import android.widget.Button;
 
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -18,7 +28,6 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.core.Size;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.videoio.VideoWriter;
 
 import java.io.File;
@@ -26,19 +35,30 @@ import java.io.File;
 import wisc.selfdriving.OpencvNativeClass;
 import wisc.selfdriving.R;
 import wisc.selfdriving.imageprocess.ImageProcess;
+import wisc.selfdriving.service.SerialPortConnection;
+import wisc.selfdriving.service.SerialPortService;
+import wisc.selfdriving.service.UDPService;
+import wisc.selfdriving.service.UDPServiceConnection;
 import wisc.selfdriving.utility.Constants;
 
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private static String TAG = "MainActivity";
+    Button startButton,testButton;
     JavaCameraView javaCameraView;
 
     VideoWriter videoWriter = null;
     private ImageProcess imageProcess = null;
 
     Mat mRgba, mGray;
-
+    ///////////////////////////
+    public int rotation = 0;
+    Handler handler = new Handler();
+    final String start = "start";
+    final String stop = "stop";
+    String Order = null;
+    int numToRotate = 5;
 
     static {
         System.loadLibrary("MyOpencvLibs");
@@ -59,9 +79,12 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        startButton = (Button) findViewById(R.id.btnstart);
+        testButton = (Button) findViewById(R.id.btntest);
 
         javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
         javaCameraView.setVisibility(View.VISIBLE);
@@ -75,8 +98,51 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         } else {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         }
+///////////////////////////////////////////////
+        setUiEnabled(false);
     }
 
+    public void Stop() {
+        super.onDestroy();
+        if (javaCameraView != null) {
+            javaCameraView.disableView();
+        }
+        stopSerialService();
+        stopUDPService();
+    }
+    public void onClickStart(View view) {
+        startUDPService();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 3000ms
+                String ip = mUDPConnection.callFunction();
+                Log.d(TAG,"SiteLocalAddress:"+ip);
+                Log.d(TAG,mUDPConnection.getOrder());
+            }
+        }, 5000);
+        setUiEnabled(true);
+        startSerialService();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver, new IntentFilter("SerialPort"));
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver, new IntentFilter("UDPserver"));
+    }
+
+    public void setUiEnabled(boolean bool) {
+        startButton.setEnabled(!bool);
+        testButton.setEnabled(bool);
+    }
+
+    public void onClickTest(View view) {
+        Log.d(TAG,"test begin");
+        String go = "throttle(1.0)";
+        if (mSerialPortConnection != null) {
+            mSerialPortConnection.sendCommandFunction(go);
+        } else {
+            Log.d(TAG, "mSerialPortConnection is null");
+        }
+    }
+///////////////////////////////////////////////
 
     protected void onPause() {
         super.onPause();
@@ -90,6 +156,8 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         if (javaCameraView != null) {
             javaCameraView.disableView();
         }
+        stopSerialService();
+        stopUDPService();
     }
 
     protected void onResume() {
@@ -128,7 +196,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         videoWriter.release();
     }
 
-
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         mRgba = inputFrame.rgba();
@@ -136,15 +203,82 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         long start = System.currentTimeMillis();
         OpencvNativeClass.convertGray(mRgba.getNativeObjAddr(), mGray.getNativeObjAddr());
         long end = System.currentTimeMillis();
-        Log.d(TAG, "It took " + (end - start) + "ms to process the image");
+        //Log.d(TAG, "It took " + (end - start) + "ms to process the image");
 
         videoWriter.write(mRgba);
 
         //get encoded image and send to server
         MatOfByte buf = imageProcess.getCompressedData(mRgba);
 
-
         return mRgba;
     }
+
+///////////////////////////////////////////////
+    //SerialPortConnection
+    private static Intent mSerial = null;
+    private static SerialPortConnection mSerialPortConnection = null;
+
+    private void startSerialService() {
+        mSerial = new Intent(this, SerialPortService.class);
+        mSerialPortConnection = new SerialPortConnection();
+        bindService(mSerial, mSerialPortConnection, Context.BIND_AUTO_CREATE);
+        startService(mSerial);
+    }
+
+    private void stopSerialService() {
+        if(mSerial != null && mSerialPortConnection != null) {
+            unbindService(mSerialPortConnection);
+            stopService(mSerial);
+            mSerial = null;
+            mSerialPortConnection = null;
+        }
+    }
+
+    //UDPConnetion
+    private static Intent mUDP = null;
+    private static UDPServiceConnection mUDPConnection = null;
+
+    private void startUDPService() {
+        Log.d(TAG, "startUDPService");
+        mUDP = new Intent(this, UDPService.class);
+        mUDPConnection = new UDPServiceConnection();
+        bindService(mUDP, mUDPConnection, Context.BIND_AUTO_CREATE);
+        startService(mUDP);
+    }
+
+    private void stopUDPService() {
+        if(mUDP != null && mUDPConnection != null) {
+            unbindService(mUDPConnection);
+            stopService(mUDP);
+            mUDP = null;
+            mUDPConnection = null;
+        }
+    }
+
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String speedAndRotation = intent.getStringExtra("speedAndRotation");
+            String rotationStatus = intent.getStringExtra("rotationStatus");
+            String stop = "throttle(0.0)";
+            
+            Gson gson = new GsonBuilder().create();
+            SerialPortService.SerialReading reading = gson.fromJson(speedAndRotation, SerialPortService.SerialReading.class);
+
+            rotation = reading.rotation_;
+            Log.d(TAG,String.valueOf(rotation));
+            mUDPConnection.sendData(String.valueOf(rotation));
+
+            if (rotation>numToRotate-1){
+                Log.d(TAG, "rotation over " + numToRotate);
+                if (mSerialPortConnection != null) {
+                    mSerialPortConnection.sendCommandFunction(stop);
+                    Log.d(TAG, "shold stop");
+                } else {
+                    Log.d(TAG, "mSerialPortConnection is null");
+                }
+            }
+        }
+    };
 
 }
